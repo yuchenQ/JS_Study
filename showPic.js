3207d7b1495f21e81c52a2fa3f833a10958df9bc
yuhcenQ <qiaoycmelbourne@gmail.com>
2018-05-21 21:00:26 +1000

    c6

--- showPic.js
+++ showPic.js
@@ -1,11 +1,48 @@
-function showPic(whichpic) {
-  // var source = whichpic.getAttribute("href");
-  // var placeholder = document.getElementById("placeholder");
-  // placeholder.src = source;
-  document.getElementById("placeholder").setAttribute("src", whichpic.getAttribute("href"));
+function prepareGallery() {
+  if (!document.getElementsByTagName
+    || !document.getElementById
+    || !document.getElementById("imagegallery"))
+    return false;
 
-  // var text = whichpic.getAttribute("title");
-  // var description = document.getElementById("description");
-  // description.firstChild.nodeValue = text;
-  document.getElementById("description").firstChild.nodeValue = whichpic.getAttribute("title");
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for (link in links) {
+    link.onclick = function() {
+      return showPic(this) ? false : true;
+    }
+  }
 }
+
+function showPic(whichpic) {
+  if (!document.getElementById("placeholder"))
+    return false;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  if(placeholder.nodeName != "IMG")
+    return false;
+  placeholder.setAttribute("src", source);
+
+  if (document.getElementById("description")) {
+    var text = whichpic.getAttribute("title")
+      ? whichpic.getAttribute("title")
+      : "";
+    var description = document.getElementById("description");
+    if (description.firstChild.nodeType == 3)
+      description.firstChild.nodeValue = text;
+    }
+    return true;
+  }
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+addLoadEvent(prepareGallery);
